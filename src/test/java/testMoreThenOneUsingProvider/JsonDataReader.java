1ba0e7dcae8cf837146799302db1a83199af073f
Mustafa Ngm <162319919+MustafaNgm@users.noreply.github.com>
2024-12-23 11:06:32 +0200

    Update JsonDataReader.java

--- src/test/java/testMoreThenOneUsingProvider/JsonDataReader.java
+++ src/test/java/testMoreThenOneUsingProvider/JsonDataReader.java
@@ -13,25 +13,22 @@ import org.json.simple.parser.ParseException;
 
 public class JsonDataReader {
 	public List<Object[]> JsonReader() throws IOException, ParseException {
-        // تحديد مسار ملف JSON
+       
         String filePath = System.getProperty("user.dir") + "/src/test/java/data/UserData.json";
 
-        // إنشاء ملف للقراءة
+       
         File srcFile = new File(filePath);
-
-        // قراءة JSON باستخدام JSONParser
+		
         JSONParser parser = new JSONParser();
         JSONArray jarray = (JSONArray) parser.parse(new FileReader(srcFile));
 
-        // إنشاء قائمة لتخزين البيانات
         List<Object[]> data = new ArrayList<>();
 
-        // قراءة كل عنصر في المصفوفة JSON
         for (Object jsonObj : jarray) {
             JSONObject person = (JSONObject) jsonObj;
 
-            // قراءة القيم من JSON
-            String firstname = (String) person.get("firstname");
+		
+		String firstname = (String) person.get("firstname");
             String middlename = (String) person.get("middlename");
             String lastname = (String) person.get("lastname");
             String email = (String) person.get("email");
